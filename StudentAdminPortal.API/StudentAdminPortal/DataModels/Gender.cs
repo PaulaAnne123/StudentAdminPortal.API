77618b0c36b553068fcf71f5e9ca341e8f222cf5
Paula McGrath <mcgrap@alum.rpi.edu>
2022-02-07 22:11:45 -0500

    Updates - Data Layer Part 1

---
+++ StudentAdminPortal.API/StudentAdminPortal/DataModels/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace StudentAdminPortal.DataModels
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
--- StudentAdminPortal.API/StudentAdminPortal/Startup.cs
+++ StudentAdminPortal.API/StudentAdminPortal/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using StudentAdminPortal.DataModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +30,7 @@ namespace StudentAdminPortal
         {
 
             services.AddControllers();
+            services.AddDbContext<StudentAdminContext>(options => options.UseSqlServer(Configuration.GetConnectionString("StudentAdminPortalDb")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "StudentAdminPortal", Version = "v1" });
